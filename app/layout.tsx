aef23889986beb860a67fe36168397b60430d776
Kevin <kevinpinn11@gmail.com>
2025-06-27 11:21:45 +0700

    update layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,16 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'Kallista Abigail',
+  description: 'Kallista Abigail is a designer and developer who creates beautiful.',
+  keywords: [
+    'Kallista Abigail',
+    'designer',
+    'developer',
+    'web design',
+    'web development',
+    'portfolio',
+  ],
   generator: 'v0.dev',
 }
 
